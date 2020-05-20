7572ba8619104b132a259a023d50c7ab5a709c5d
AkankshaRakeshJain <63910223+AkankshaRakeshJain@users.noreply.github.com>
2020-05-20 19:08:01 +0530

    Update SUMTRIAN_Sums in a Triangle.py

--- SUMTRIAN_Sums in a Triangle.py
+++ SUMTRIAN_Sums in a Triangle.py
@@ -1,4 +1,4 @@
-for number_of_rows in range(1):
+for number_of_rows in range(int(input()):
     number_of_rows = int(input())
     lst = []
     for i in range(number_of_rows):
@@ -8,7 +8,7 @@ for number_of_rows in range(1):
     for i in range(number_of_rows-1, 0, -1):
         for j in range(i):
             if lst[i][j] > lst[i][j+1]:
-                lst[i-1][j] += + lst[i][j]
+                lst[i-1][j] += lst[i][j]
             else:
                 lst[i-1][j] += lst[i][j+1]
     print(lst[0][0])
